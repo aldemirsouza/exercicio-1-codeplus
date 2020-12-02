e812e9d9ba9d962bcfe12bdcc7aac89a83158fa0
Aldemir Souza <aldemmir17b@hotmail.com>
2020-12-01 21:42:14 -0300

    Exercicio-1 concluido

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const eplusDevList = ['Parça', 'Galego', 'Xará', 'Baqueta', 'Na', 'Lindo', 'Pablito', 'Bahia']
 
-function listDevs(list){
+const $eplusList = document.querySelector('.eplusList');
 
+function listDevs(list){
+    eplusDevList.forEach(function(dev){
+        const itemDev = document.createElement('div');
+        itemDev.innerText = dev;
+        console.log(itemDev);
+    });
 }
 
-listDevs(eplusDevList)
+listDevs(eplusDevList);
 
 //a função deve dar console.log em cada um dos desenvolvedores da e-plus.
